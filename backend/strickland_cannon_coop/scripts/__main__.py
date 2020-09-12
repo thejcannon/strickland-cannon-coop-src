952d14057ef085d8f2eb8fadf75c2904eeea5a00
Joshua Cannon <joshua.cannon@ni.com>
2020-09-12 17:21:47 -0500

    Disable mortgage

--- backend/strickland_cannon_coop/scripts/__main__.py
+++ backend/strickland_cannon_coop/scripts/__main__.py
@@ -1,9 +1,9 @@
 from strickland_cannon_coop.scripts.electricity import main as electricity
-from strickland_cannon_coop.scripts.mortgage import main as mortgage
+# from strickland_cannon_coop.scripts.mortgage import main as mortgage
 from strickland_cannon_coop.scripts.water import main as water
 
 
 if __name__ == "__main__":
-    electricity()
-    mortgage()
+    # mortgage()
     water()
+    electricity()
